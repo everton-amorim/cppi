ce78f978bcc8f855940cd4c693563905ece23d81
Everton Amorim <everton@evertonamorim.com.br>
2017-09-12 19:00:30 -0300

    PM2

---
+++ pm2.config.js
@@ -0,0 +1,11 @@
+'use strict';
+const fs = require('fs');
+
+const defaultCwd = '~/cppi';
+let cwd = fs.existsSync(defaultCwd) ? defaultCwd : './';
+
+module.exports = [{
+    name: 'cppi',
+    script: './bin/www',
+    cwd
+}];
